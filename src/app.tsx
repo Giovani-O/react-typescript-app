e3a3d86323eb0d182c0b6705380aa637f9c3bdc7
Giovani <giovani98@proton.me>
2024-05-03 21:16:53 -0300

    feat: Imports component

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,18 @@
+import { FirstComponent } from "./assets/components/firstComponent"
+
 export function App() {
-  return (<h1>Typescript com React</h1>)
+  const name: string = 'Giovani'
+
+  const greetUser = (name: string): string => {
+    return `Olá ${name}`
+  }
+
+  return (
+    <div>
+      <h1>Typescript com React</h1>
+      <h2>Name: {name}</h2>
+      <h3>{greetUser(name)}</h3>
+      <FirstComponent />
+    </div>
+  )
 }
